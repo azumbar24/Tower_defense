3ac2b437804ee2524553d3f0ccfea12027a2282e
Leviathan59 <115638538+azumbar24@users.noreply.github.com>
2022-11-23 08:45:50 -0500

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,5 +1,5 @@
 class Settings:
-    crusader_speed = .5
+    knight_speed = .5
 
     def __init__(self):
         """Initialize the game's settings."""
@@ -7,9 +7,8 @@ class Settings:
         # Screen Settings
         self.screen_width = 1200
         self.screen_height = 800
-        self.bg_color = (230, 230, 230)
 
-        # crusader Settings
-        self.crusader_speed = 1.5
-        self.crusader_limit = 3
+        # knight Settings
+        self.knight_speed = 1.5
+        self.knight_limit = 3
 
