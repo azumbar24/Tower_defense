c8df142c76456dcfa5b6bd46ba293b5b3538e089
Leviathan59 <115638538+azumbar24@users.noreply.github.com>
2022-11-22 23:36:29 -0500

    Create settings.py

---
+++ settings.py
@@ -0,0 +1,15 @@
+class Settings:
+    crusader_speed = .5
+
+    def __init__(self):
+        """Initialize the game's settings."""
+
+        # Screen Settings
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
+
+        # crusader Settings
+        self.crusader_speed = 1.5
+        self.crusader_limit = 3
+
