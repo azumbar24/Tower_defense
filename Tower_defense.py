593d761ffdc320301071e70e82f6caebf006de27
Leviathan59 <115638538+azumbar24@users.noreply.github.com>
2022-11-27 21:46:59 -0500

    Update Tower_defense.py

--- Tower_defense.py
+++ Tower_defense.py
@@ -2,6 +2,7 @@ import pygame
 import sys
 import time
 from knight import Knight
+from settings import Settings
 
 TILE_SIZE = 64
 WINDOW_SIZE = 15 * TILE_SIZE
@@ -11,16 +12,15 @@ tower = pygame.image.load("assets/towerAlt.png")
 tower_rect = tower.get_rect()
 background = pygame.image.load("assets/backgroundColorGrass.png")
 background_rect = background.get_rect()
-#knight = pygame.image.load("assets/Knight.png")
-#knight_rect = knight.get_rect()
+knight = pygame.image.load("assets/Knight.png")
+knight_rect = knight.get_rect()
 knight1 = pygame.image.load("assets/Knight1.png")
 knight1_rect = knight1.get_rect()
 
 screen_rect = screen.get_rect()
 
 num_tiles = screen_rect.width // tower_rect.width
-screen.blit(background, (0, 50))
-screen.blit(tower, (400, 475))
+
 #screen.blit(knight, (500, 600))
 screen.blit(knight1, (300, 600))
 
@@ -40,8 +40,18 @@ class TowerDefense:
 
         self.knight = Knight(self)
 
+    def run_game(self):
+        """Start the main loop for the game"""
+        while True:
+            self._check_events()
+
+            self.knight.update()
+            screen.blit(background, (130, 20))
+            screen.blit(tower, (600, 430))
+            self._update_screen()
+
     def _check_events(self):
-        """Respond to keypresses"""
+        """Respond to key presses"""
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
@@ -51,7 +61,7 @@ class TowerDefense:
                 self._check_keyup_events(event)
 
     def _check_keydown_events(self, event):
-        """Respond to keypresses."""
+        """Respond to key presses."""
         if event.key == pygame.K_RIGHT:
             self.knight.moving_right = True
         elif event.key == pygame.K_LEFT:
@@ -67,16 +77,20 @@ class TowerDefense:
             elif event.key == pygame.K_LEFT:
                 self.knight.moving_left = False
 
+    def _update_screen(self):
+        # Update images on the screen, and flip to the new scren.
+        self.knight.blitme()
+
+        # make the most recently drawn screen visisble.
+        pygame.display.flip()
 
 
 
 
+    # clock = pygame.tick.Clock()
+    # clock.tick(60)
 
-#clock = pygame.tick.Clock()
-
-pygame.init()
-
-pygame.display.flip()
-#clock.tick(60)
-time.sleep(7)
-
+if __name__=='__main__':
+    #make a game instance, and run the game.
+    ai = TowerDefense()
+    ai.run_game()
