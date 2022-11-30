fa4e960ea619607f42a9bd8919c93e8b38d88eeb
Leviathan59 <115638538+azumbar24@users.noreply.github.com>
2022-11-30 09:00:21 -0500

    Update goblin.py

--- goblin.py
+++ goblin.py
@@ -2,7 +2,7 @@ import pygame
 from pygame.sprite import Sprite
 
 class Goblin(Sprite):
-    "A class to represent a goblin"
+    """A class to represent a goblin"""
 
     def __init__(self, ai_game):
         "Initializing the goblin and set its starting position"
@@ -10,20 +10,20 @@ class Goblin(Sprite):
         self.screen = ai_game.screen
         self.settings = ai_game.settings
 
-        #Load the goblin image and set its rect attribute
+        # Load the goblin image and set its rect attribute
         self.image = pygame.image.load('assets/goblin.png')
         self.rect = self.image.get_rect()
 
-        #Start each goblin near the top left of the screen
+        # Start each goblin near the top left of the screen
         self.rect.x = self.rect.width
         self.rect.y = self.rect.height
 
-        #store the goblin exact horizontal position
+        # store the goblin exact horizontal position
         self.x = float(self.rect.x)
 
     def update(self):
         """Move the goblin to the right or left"""
-        self.x -= 1
+        self.x += (self.settings.goblin_speed * self.settings.fleet_direction)
         self.rect.x = self.x
     def check_edges(self):
         """return true if goblin is at the edge of the screen"""
