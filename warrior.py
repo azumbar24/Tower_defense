9e7aa0bc034d7876796d182e3fbbf16af40152bf
Leviathan59 <115638538+azumbar24@users.noreply.github.com>
2022-12-01 08:56:46 -0500

    Create warrior.py

---
+++ warrior.py
@@ -0,0 +1,44 @@
+import pygame
+
+class Warrior:
+
+    def __init__(self, ai_game):
+        """Warrior's starting position"""
+        self.screen = ai_game.screen
+        self.speed = 2
+
+        # Loading the warrior image
+        self.image = pygame.image.load('assets/Warrior.png')
+        self.rect = self.image.get_rect()
+
+        # Warrior's starting position is at the middle bottom of the screen
+        self.rect.midbottom = self.screen.get_rect().midbottom
+
+        # store a decimal value for the warriors horizontal position
+        self.x = float(self.rect.x)
+
+        # Movement flags
+        self.moving_right = False
+        self.moving_left = False
+
+    def update(self):
+        """Update the warrior's position based on movement flag"""
+        # Update the warrior's x value, not the rect
+        if self.moving_right and self.rect.right < self.screen.get_rect().right:
+            self.x += self.speed
+            self.image = pygame.image.load('assets/Warrior.png')
+        if self.moving_left and self.rect.left > 0:
+            self.x -= self.speed
+            self.image = pygame.image.load('assets/Warrior1.png')
+
+        # Update rect object from self.x
+        self.rect.x = self.x
+
+    def blitme(self):
+        """Draw the warrior at its current location."""
+        self.screen.blit(self.image, self.rect)
+
+    def center_warrior(self):
+        """center the warrior on the screen"""
+        self.rect.midbottom = self.screen_rect.midbottom
+        self.x = float(self.rect.x)
